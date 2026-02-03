e4f81078fd2a14b738622b95ff861c6c02a6d8d7
Mamungithube <orroshidmdmamun50@gmail.com>
2026-02-03 06:14:48 +0600

    custom model ctreate

--- account/models.py
+++ account/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils import timezone
+from datetime import timedelta 
 
 # Create your models here.
+
+def get_expiry():
+    return timezone.now() + timedelta(days=365)
+
+
+"""---------Custom User Manager---------"""
+class UserManager(BaseUserManager):
+    def create_user(self, email,password = None , **extra_fields):
+        if not email:
+            raise ValueError('The Email must be provided')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password = None , **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+    
+
+"""---------Custom User Model---------"""
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    address = models.TextField(blank=True, null=True)
+    membership_expiry = models.DateTimeField(default=get_expiry)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.full_name or self.email
+    
+
+"""---------Profile Model---------"""
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=4, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    interests = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.full_name}"
