0e51ea4a494fc6cf0f0f59046d62cf4c3a129230
Mamungithube <orroshidmdmamun50@gmail.com>
2026-05-25 20:06:15 +0600

    PressCoverage

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PressCoverageListView
 
 urlpatterns = [
-    path('', PressCoverageListView.as_view(), name='press-list'),
+    path('PressCoverage', PressCoverageListView.as_view(), name='press-list'),
 ]
