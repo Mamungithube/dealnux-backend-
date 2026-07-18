edf99977e571df304dab2291e75e5be76ce730da
root <root@srv1351261.hstgr.cloud>
2026-07-18 09:25:22 +0000

    Add is_active field to SellerProduct for soft delete

--- store/models.py
+++ store/models.py
@@ -249,6 +249,7 @@ class SellerProduct(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ['-created_at']
